bd6ebff3ab2f7e600ac1a5b8e0c17ff8902fcaa1
Daniel Albino <daniel.albino@danielsoftdev.com>
2023-09-21 10:28:17 +0100

    added sprites

--- Assets/Scripts/CharacterBody2D.gd
+++ Assets/Scripts/CharacterBody2D.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var spr = $AnimatedSprite2D
 @export var speed = 300
 @export var jumpForce = 150
 @export var gravity = 200
@@ -18,16 +19,24 @@ func _physics_process(delta):
 	# Horizontal Movements
 	if Input.is_action_pressed("Left"):
 		velocity.x = -speed
+		spr.play("Run")
+		spr.flip_h = true
 	elif Input.is_action_pressed("Right"):
 		velocity.x = speed
+		spr.play("Run")
+		spr.flip_h = false
 	else:
 		velocity.x = 0
+		spr.play("Idle")
 		
 	# Jump Movement
 	if Input.is_action_pressed("Jump"):
 		if onGround:
 			velocity.y = -jumpForce
 			onGround = false
+	
+	if !onGround:
+		spr.play("Jump")	
 	
 		
 	if is_on_floor():
